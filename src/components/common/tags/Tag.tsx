60f5590917b537e85f8384ac6ede75eec6a018ef
dchnsk <leriidchnsk@gmail.com>
2022-11-23 00:44:55 +0300

    Rewrite Tag component using tailwind

--- src/components/common/tags/Tag.tsx
+++ src/components/common/tags/Tag.tsx
@@ -1,16 +1,22 @@
 import { ITextTag } from '../../../@types/tags';
-import styles from '@styles/tags/tags.module.scss';
 const randomColor = require('randomcolor'); //* unfortunutely, this lib doesnt support import/export
 
 interface ITextTagProps extends ITextTag {
+    value: string;
+    color?: string;
     isActive?: boolean;
 }
 
 export const TextTag = ({ value, color = randomColor(), isActive = false }: ITextTagProps): JSX.Element => {
     return (
         <span
-            className={styles.text_tag}
-            style={{ color: color, border: `2px solid ${color}`, cursor: isActive ? 'pointer' : 'default' }}
+            style={{ color: color, borderColor: color }}
+            className={`
+                p-1 mr-1 
+                text-center
+                rder-solid border-2 rounded-md 
+                ${isActive ? 'cursor-pointer' : 'cursor-default'}
+            `}
         >
             {value}
         </span>
